dff2bdfdba748424e807c76415e6a5c19869370a
sayaksengupta <sayak.sengupta@atpl.live>
2024-09-04 00:36:44 +0530

    attendance schema update

--- models/UserModel/Attendance.js
+++ models/UserModel/Attendance.js
@@ -1,20 +1,26 @@
 const mongoose = require("mongoose");
 
-const attendanceSchema = new mongoose.Schema({
+const attendanceSchema = new mongoose.Schema(
+  {
     userName: { type: String },
     userEmail: { type: String },
-    currentDate: { type: String },
-    punchin: { type: String },
-    punchOut: { type: String },
-    time: { type: String },
+    currentDate: { type: Date },
+    punchIn: { type: Date },
+    punchOut: { type: Date },
+    workingTime: { type: Number, default: 0 },
     ip: { type: String },
-    status: { type: String },
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "register user",
-    required: true,
+    status: {
+      type: String,
+      enum: ["On Time", "Late"],
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "register user",
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const AttendanceModel = mongoose.model("attendance", attendanceSchema);
 
